c4b0212f6e4ef4d53930da1b040f505b69333888
0KrakerS0 <pawel.krakowiak.93@gmail.com>
2016-01-14 01:16:06 +0100

    Dodanie ikonek dla typow znacznikow

--- app/src/main/java/st/pawel/mobilnyprzewodnik/map/model/MarkerType.java
+++ app/src/main/java/st/pawel/mobilnyprzewodnik/map/model/MarkerType.java
@@ -12,26 +12,26 @@ public enum MarkerType {
 
     //TODO Dodac wlasciwe resource dla ikonek
     @SerializedName(Metadata.ANTIQUE)
-    ANTIQUE(R.drawable.ic_menu_maps_map, R.string.marker_type_antique),
+    ANTIQUE(R.drawable.ic_antique, R.string.marker_type_antique),
     @SerializedName(Metadata.MUSEUM)
-    MUSEUM(R.drawable.ic_menu_maps_map, R.string.marker_type_museum),
+    MUSEUM(R.drawable.ic_museum, R.string.marker_type_museum),
     @SerializedName(Metadata.MONUMENT)
-    MONUMENT(R.drawable.ic_menu_maps_map, R.string.marker_type_monument),
+    MONUMENT(R.drawable.ic_monument, R.string.marker_type_monument),
     @SerializedName(Metadata.GALLERY)
-    GALLERY(R.drawable.ic_menu_maps_map, R.string.marker_type_gallery),
+    GALLERY(R.drawable.ic_gallery, R.string.marker_type_gallery),
     @SerializedName(Metadata.RESTAURANT)
-    RESTAURANT(R.drawable.ic_menu_maps_map, R.string.marker_type_restaurant),
+    RESTAURANT(R.drawable.ic_restauant, R.string.marker_type_restaurant),
     @SerializedName(Metadata.TOURIST_ATTRACTION)
-    TOURIST_ATTRACTION(R.drawable.ic_menu_maps_map, R.string.marker_type_tourist_attraction),
+    TOURIST_ATTRACTION(R.drawable.ic_turistic_attraction, R.string.marker_type_tourist_attraction),
     @SerializedName(Metadata.HOTEL)
-    HOTEL(R.drawable.ic_menu_maps_map, R.string.marker_type_museum),
+    HOTEL(R.drawable.ic_hotel, R.string.marker_type_museum),
     @SerializedName(Metadata.CASH_MACHINE)
-    CASH_MACHINE(R.drawable.ic_menu_maps_map, R.string.marker_type_cash_machine),
+    CASH_MACHINE(R.drawable.ic_cash_machine, R.string.marker_type_cash_machine),
     @SerializedName(Metadata.FAST_FOOD)
-    FAST_FOOD(R.drawable.ic_menu_maps_map, R.string.marker_type_fast_food),
+    FAST_FOOD(R.drawable.ic_fast_food, R.string.marker_type_fast_food),
     @SerializedName(Metadata.TOURIST_INFORMATION)
-    TOURIST_INFORMATION(R.drawable.ic_menu_maps_map, R.string.marker_type_tourist_information),
-    UNKNOWN(R.drawable.ic_menu_maps_map, R.string.marker_type_unknown),
+    TOURIST_INFORMATION(R.drawable.ic_tourist_information, R.string.marker_type_tourist_information),
+    UNKNOWN(R.drawable.ic_turistic_attraction, R.string.marker_type_unknown),
 
     ;
 
